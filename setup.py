9f71fd7d173a46915f0d92ee31161746ec3f9c51
Feromond <Jacobpmish@gmail.com>
2024-10-19 14:55:14 -0600

    updated setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hypixel-api-lib',  
+    version='0.1.0',
+    author='Desmond O, Jacob M',
+    author_email='JacobPMish@gmail.com, desmond.obrien@ucalgary.ca',
+    description='A python library to interact with the hypixel api, specifically focused on skyblock',
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    url='https://github.com/feromond/hypixel-api-lib',
+    packages=find_packages(exclude=['tests']),
+    # install_requires=[
+    # ],
+)
