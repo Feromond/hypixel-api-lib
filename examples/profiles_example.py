854965d167f41178bffd97a823cd10ae3c99d743
Feromond <Jacobpmish@gmail.com>
2024-11-04 15:15:35 -0700

    Temp examples file used for building more functionality

--- examples/profiles_example.py
+++ examples/profiles_example.py
@@ -2,29 +2,28 @@ from hypixel_api_lib.Profiles import SkyBlockProfiles
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from a .env file
 load_dotenv()
 
 api_key = os.getenv("HYPIXEL_API_KEY")
-player_uuid = "be5c42d4ea454601a57b944c6413f3cd"  # Replace with the player's UUID
+player_uuid = "be5c42d4ea454601a57b944c6413f3cd"  # Feromond
 
 profiles_api = SkyBlockProfiles(api_key)
 
 # Fetch all profiles for the player
-# try:
-#     profiles = profiles_api.get_profiles_by_player_uuid(player_uuid)
-#     print(f"Found {len(profiles)} profiles for player UUID {player_uuid}")
-#     for profile in profiles:
-#         print(profile)
-#         # Access members
-#         for member_uuid in profile.list_member_uuids():
-#             member = profile.get_member(member_uuid)
-#             print(member)
-#             # Access member attributes
-#             print(f"  Player Stats: {member.player_stats}")
-#             # Add additional processing as needed
-# except Exception as e:
-#     print(f"Error: {e}")
+try:
+    profiles = profiles_api.get_profiles_by_player_uuid(player_uuid)
+    print(f"Found {len(profiles)} profiles for player UUID {player_uuid}")
+    for profile in profiles:
+        print(profile)
+        # Access members
+        for member_uuid in profile.list_member_uuids():
+            member = profile.get_member(member_uuid)
+            print(member)
+            # Access member attributes
+            print(f"  Player Stats: {member.player_stats}")
+            # Add additional processing as needed
+except Exception as e:
+    print(f"Error: {e}")
 
 # Fetch the selected profile for the player
 try:
@@ -32,8 +31,8 @@ try:
     if selected_profile:
         print(f"Selected profile for player UUID {player_uuid}: {selected_profile}")
 
-        feromond = selected_profile.get_member(player_uuid)
-        print(feromond.events)
+        # feromond = selected_profile.get_member(player_uuid)
+        # print(feromond.events)
     else:
         print(f"No selected profile found for player UUID {player_uuid}")
 except Exception as e:
