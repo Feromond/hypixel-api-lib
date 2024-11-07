50fbbca5b167f6c04bcdba22a72974821210c7ec
Jacob Mish <Jacobpmish@gmail.com>
2024-11-07 15:28:44 -0700

    Added type hints to currencies within the profile member component

--- hypixel_api_lib/member/Currencies.py
+++ hypixel_api_lib/member/Currencies.py
@@ -7,14 +7,14 @@ class Essence:
         current (int): The current amount of this essence.
     """
 
-    def __init__(self, essence_type, data):
-        self.essence_type = essence_type
-        self.current = data.get('current', 0)
+    def __init__(self, essence_type: str, data: dict) -> None:
+        self.essence_type: str = essence_type
+        self.current: int = data.get('current', 0)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.essence_type} Essence: {self.current}"
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
 
 class Currencies:
@@ -27,23 +27,23 @@ class Currencies:
         essence (dict of str to Essence): A dictionary of essences by type.
     """
 
-    def __init__(self, data):
-        self.coin_purse = data.get('coin_purse', 0.0)
-        self.motes_purse = data.get('motes_purse', 0.0)
+    def __init__(self, data: dict) -> None:
+        self.coin_purse: float = data.get('coin_purse', 0.0)
+        self.motes_purse: float = data.get('motes_purse', 0.0)
 
-        essence_data = data.get('essence', {})
-        self.essence = {
+        essence_data: dict = data.get('essence', {})
+        self.essence: dict[str,Essence] = {
             essence_type: Essence(essence_type, essence_info)
             for essence_type, essence_info in essence_data.items()
         }
 
-        known_fields = ['coin_purse', 'motes_purse', 'essence']
-        self.additional_currencies = {k: v for k, v in data.items() if k not in known_fields}
+        known_fields: list[str] = ['coin_purse', 'motes_purse', 'essence']
+        self.additional_currencies: dict[str,str] = {k: v for k, v in data.items() if k not in known_fields}
 
-    def __str__(self):
+    def __str__(self) -> str:
         essences_str = ', '.join([str(e) for e in self.essence.values()])
         return (f"Currencies(Coin Purse: {self.coin_purse}, Motes Purse: {self.motes_purse}, "
                 f"Essences: {essences_str})")
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__str__()
