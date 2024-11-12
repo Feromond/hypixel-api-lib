10583362bd7d4a6eeb58ea8a43396036387b681e
Jacob Mish <Jacobpmish@gmail.com>
2024-11-12 11:45:06 -0700

    Added dungeon journal and daily runs to the dungeons component

--- hypixel_api_lib/member/dungeons/Dungeons.py
+++ hypixel_api_lib/member/dungeons/Dungeons.py
@@ -1,4 +1,6 @@
 from .PlayerClasses import PlayerClasses
+from .DungeonJournal import DungeonJournal
+from .DailyRuns import DailyRuns
 
 class Dungeons():
     """
@@ -13,10 +15,10 @@ class Dungeons():
 
         self.dungeon_types: dict = dungeons_data.get("dungeon_types", {})                       # Huge
         self.player_classes: PlayerClasses = PlayerClasses(dungeons_data.get("player_classes", {})) 
-        self.dungeon_journal: dict[str,list[str]] = dungeons_data.get("dungeon_journal", {}) 
+        self.dungeon_journal: DungeonJournal = DungeonJournal(dungeons_data.get("dungeon_journal", {}))
         self.dungeons_blah_blah: list[str] = dungeons_data.get("dungeons_blah_blah", [])
         self.selected_dungeon_class: str | None = dungeons_data.get("selected_dungeon_class", None)
-        self.daily_runs: dict[str,int] = dungeons_data.get("daily_runs", {})
+        self.daily_runs: DailyRuns = DailyRuns(dungeons_data.get("daily_runs", {}))
         self.treasures: dict = dungeons_data.get("treasures", {})                               # Huge
         self.dungeon_hub_race_settings: dict[str,str] = dungeons_data.get("dungeon_hub_race_settings", {}) 
         self.last_dungeon_run: str | None = dungeons_data.get("last_dungeon_run", None)
