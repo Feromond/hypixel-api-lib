5eca6ea470baee0bb1e30125dcd98b2a21acbab9
Jacob Mish <Jacobpmish@gmail.com>
2024-11-12 11:44:48 -0700

    Created the simple DailyRuns data class

---
+++ hypixel_api_lib/member/dungeons/DailyRuns.py
@@ -0,0 +1,13 @@
+class DailyRuns():
+    """
+    DailyRuns data class to handle the dungeon daily runs component
+
+    Attributes:
+        current_day_stamp (int): the current day timestamp for the data (0 if none)
+        completed_runs_count (int): number of runs completed for the current day
+    """
+    def __init__(self, data: dict[str,int]) -> None:
+        self.current_day_stamp: int = data.get("current_day_stamp", 0)
+        self.completed_runs_count: int = data.get("completed_runs_count", 0)
+    def __str__(self) -> str:
+        return f"Current Day Stamp: {self.current_day_stamp}, Completed Runs Count: {self.completed_runs_count}"
