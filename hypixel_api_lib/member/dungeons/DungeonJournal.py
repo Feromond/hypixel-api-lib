7fd31b1701dab389eb43c2f718d059dbd72877bb
Jacob Mish <Jacobpmish@gmail.com>
2024-11-12 11:44:29 -0700

    Created the simple DungeonJournal data class

---
+++ hypixel_api_lib/member/dungeons/DungeonJournal.py
@@ -0,0 +1,12 @@
+class DungeonJournal():
+    """
+    DungeonJournal data class to handle the data within the dungeon journals.
+
+    Attributes:
+        unlocked_journals (list[str]): list of unlocked journals
+    """
+    def __init__(self, data: dict[str,list[str]]) -> None:
+        self.unlocked_journals: list[str] = data.get("unlocked_journals", {})
+
+    def __str__(self) -> str:
+        return f"Unlocked Journals: {self.unlocked_journals}"
